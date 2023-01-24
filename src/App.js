5eee61b55d25e50d8445458cb2cd8a949e79a081
n6-xcode <121912919+n6-xcode@users.noreply.github.com>
2023-01-24 11:40:38 +0700

    Revert "ubahan ikhlasdansantai"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import "./App.css";
-// import { Component } from "react";
-// import { Route, Routes } from "react-router-dom";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Contact from "./components/Contact";
@@ -10,28 +8,16 @@ import Drive from "./components/Drive";
 import Card from "./components/Card";
 
 function App() {
-  const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/about", element: <About /> },
-    { path: "/contact", element: <Card /> },
-    { path: "/drive", element: <Drive /> },
-  ]);
   return (
     <>
-      {/* <h2 className="text-[96px] text-black">Hello World</h2> */}
       <Navbar />
-      <RouterProvider router={router} />
+      <Routes>
+        <Route exact path="/profile" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact Card={Card} />} />
+        <Route path="/drive" element={<Drive />} />
+      </Routes>
     </>
-    // <>
-    //   <Navbar />
-    //   <Card></Card>
-    //   <Routes>
-    //     <Route exact path="/profile" element={<Home />} />
-    //     <Route path="/about" element={<About />} />
-    //     <Route path="/contact" element={<Contact Card={Card} />} />
-    //     <Route path="/drive" element={<Drive />} />
-    //   </Routes>
-    // </>
   );
 }
 
